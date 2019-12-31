871197b7c41c8055cce6e6a6b8d69eefa4e7d8e2
ricky wahba <rjwahba@gmail.com>
2019-12-31 16:17:11 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,8 +3,8 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-    array.length.times do |count|
-      if array[count] == value_to_find
+  array.length.times do |count|
+    if array[count] == value_to_find
         return count
       end
 end
