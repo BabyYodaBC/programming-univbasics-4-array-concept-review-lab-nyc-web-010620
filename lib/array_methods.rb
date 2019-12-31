fb7905200f01e3f9314fc6af7bf3abbcbe0d3d4a
ricky wahba <rjwahba@gmail.com>
2019-12-31 16:13:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,8 +3,11 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-    array.length.times do |index|
-      
+    array.length.times do |count|
+      if array[count] == value_to_find
+        return count
+      end
+      nil
 end
 
 def find_min_value(array)
