50352d583d10f9f4679a897438ead3cf005a8994
ricky wahba <rjwahba@gmail.com>
2019-12-31 16:18:59 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,12 +3,13 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.length.times do |count|
-    if array[count] == value_to_find
-        return count
-      end
+  x = array[0]
+  array.length.times do |index|
+    if array[index] > x 
+      x = array[index]
 end
 end
+
 
 def find_min_value(array)
   # Add your solution here
